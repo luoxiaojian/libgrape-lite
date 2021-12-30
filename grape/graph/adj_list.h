5253b45232ab73c5f8cdae5bf57151cff549b892
晓简 <lxj193371@alibaba-inc.com>
2021-12-30 19:54:42 +0800

    Implemented MutableEdgecutFragment.

--- grape/graph/adj_list.h
+++ grape/graph/adj_list.h
@@ -49,22 +49,17 @@ struct Nbr {
     return *this;
   }
 
-  void GetEdgeSrc(const Edge<VID_T, EDATA_T>& edge) {
-    neighbor.SetValue(edge.src());
-    data = edge.edata();
-  }
-
-  void GetEdgeDst(const Edge<VID_T, EDATA_T>& edge) {
-    neighbor.SetValue(edge.dst());
-    data = edge.edata();
-  }
-
   Vertex<VID_T> get_neighbor() const { return neighbor; }
   EDATA_T get_data() const { return data; }
 
   Vertex<VID_T> neighbor;
   EDATA_T data;
 };
+
+template <typename VID_T, typename EDATA_T>
+bool operator<(const Nbr<VID_T, EDATA_T>& lhs, const Nbr<VID_T, EDATA_T>& rhs) {
+  return lhs.neighbor.GetValue() < rhs.neighbor.GetValue();
+}
 
 /**
  * @brief A neighbor of a vertex in the graph. (partial specialization with
@@ -85,14 +80,6 @@ struct Nbr<VID_T, EmptyType> {
   Nbr& operator=(const Nbr& rhs) {
     neighbor = rhs.neighbor;
     return *this;
-  }
-
-  void GetEdgeSrc(const Edge<VID_T, EmptyType>& edge) {
-    neighbor.SetValue(edge.src());
-  }
-
-  void GetEdgeDst(const Edge<VID_T, EmptyType>& edge) {
-    neighbor.SetValue(edge.dst());
   }
 
   Vertex<VID_T> get_neighbor() const { return neighbor; }
@@ -254,6 +241,182 @@ class AdjList {
   NbrT* end_;
 };
 
+template <typename VID_T, typename EDATA_T, typename PRED_T>
+class FilterAdjList {
+  using NbrT = Nbr<VID_T, EDATA_T>;
+
+ public:
+  FilterAdjList(NbrT* b, NbrT* e, const PRED_T& pred)
+      : begin_(b), end_(e), pred_(pred) {
+    while (begin_ != end_ && !pred_(*begin_)) {
+      ++begin_;
+    }
+  }
+  ~FilterAdjList() {}
+
+  inline bool Empty() const { return begin_ == end_; }
+
+  inline bool NotEmpty() const { return !Empty(); }
+
+  class iterator {
+    using pointer_type = NbrT*;
+    using reference_type = NbrT&;
+
+   private:
+    NbrT* current_;
+    NbrT* end_;
+    const PRED_T& pred_;
+
+   public:
+    explicit iterator(const pointer_type& c, const pointer_type& end,
+                      const PRED_T& pred) noexcept
+        : current_(c), end_(end), pred_(pred) {}
+    reference_type operator*() const noexcept { return *current_; }
+    pointer_type operator->() const noexcept { return current_; }
+
+    iterator& operator++() noexcept {
+      do {
+        ++current_;
+      } while ((current_ != end_) && !pred_(*current_));
+      return *this;
+    }
+
+    iterator operator++(int) noexcept {
+      NbrT* old_current = current_;
+      do {
+        ++current_;
+      } while ((current_ != end_) && !pred_(*current_));
+      return iterator(old_current, end_, pred_);
+    }
+
+    bool operator==(const iterator& rhs) noexcept {
+      return current_ == rhs.current_;
+    }
+    bool operator!=(const iterator& rhs) noexcept {
+      return current_ != rhs.current_;
+    }
+  };
+
+  class const_iterator {
+    using pointer_type = const NbrT*;
+    using reference_type = const NbrT&;
+
+   private:
+    const NbrT* current_;
+    const NbrT* end_;
+    const PRED_T& pred_;
+
+   public:
+    explicit const_iterator(const pointer_type& c, const pointer_type& end,
+                            const PRED_T& pred) noexcept
+        : current_(c), end_(end), pred_(pred) {}
+    reference_type operator*() const noexcept { return *current_; }
+    pointer_type operator->() const noexcept { return current_; }
+
+    const_iterator& operator++() noexcept {
+      do {
+        ++current_;
+      } while ((current_ != end_) && !pred_(*current_));
+      return *this;
+    }
+
+    const_iterator operator++(int) noexcept {
+      const NbrT* old_current = current_;
+      do {
+        ++current_;
+      } while ((current_ != end_) && !pred_(*current_));
+      return const_iterator(old_current, end_, pred_);
+    }
+
+    bool operator==(const const_iterator& rhs) noexcept {
+      return current_ == rhs.current_;
+    }
+    bool operator!=(const const_iterator& rhs) noexcept {
+      return current_ != rhs.current_;
+    }
+  };
+
+  iterator begin() { return iterator(begin_, end_, pred_); }
+  iterator end() { return iterator(end_, end_, pred_); }
+
+  const_iterator begin() const { return const_iterator(begin_, end_, pred_); }
+  const_iterator end() const { return const_iterator(end_, end_, pred_); }
+
+  bool empty() const { return end_ == begin_; }
+
+ private:
+  NbrT* begin_;
+  NbrT* end_;
+  const PRED_T& pred_;
+};
+
+template <typename VID_T, typename EDATA_T, typename PRED_T>
+class FilterConstAdjList {
+  using NbrT = Nbr<VID_T, EDATA_T>;
+
+ public:
+  FilterConstAdjList(const NbrT* b, const NbrT* e, const PRED_T& pred)
+      : begin_(b), end_(e), pred_(pred) {
+    while (begin_ != end_ && !pred_(*begin_)) {
+      ++begin_;
+    }
+  }
+  ~FilterConstAdjList() {}
+
+  inline bool Empty() const { return begin_ == end_; }
+
+  inline bool NotEmpty() const { return !Empty(); }
+
+  class const_iterator {
+    using pointer_type = const NbrT*;
+    using reference_type = const NbrT&;
+
+   private:
+    const NbrT* current_;
+    const NbrT* end_;
+    const PRED_T& pred_;
+
+   public:
+    explicit const_iterator(const pointer_type& c, const pointer_type& end,
+                            const PRED_T& pred) noexcept
+        : current_(c), end_(end), pred_(pred) {}
+    reference_type operator*() const noexcept { return *current_; }
+    pointer_type operator->() const noexcept { return current_; }
+
+    const_iterator& operator++() noexcept {
+      do {
+        ++current_;
+      } while ((current_ != end_) && !pred_(*current_));
+      return *this;
+    }
+
+    const_iterator operator++(int) noexcept {
+      const NbrT* old_current = current_;
+      do {
+        ++current_;
+      } while ((current_ != end_) && !pred_(*current_));
+      return const_iterator(old_current, end_, pred_);
+    }
+
+    bool operator==(const const_iterator& rhs) noexcept {
+      return current_ == rhs.current_;
+    }
+    bool operator!=(const const_iterator& rhs) noexcept {
+      return current_ != rhs.current_;
+    }
+  };
+
+  const_iterator begin() const { return const_iterator(begin_, end_, pred_); }
+  const_iterator end() const { return const_iterator(end_, end_, pred_); }
+
+  bool empty() const { return end_ == begin_; }
+
+ private:
+  const NbrT* begin_;
+  const NbrT* end_;
+  const PRED_T& pred_;
+};
+
 /**
  * @brief A immutable iteratable adjencent list of a vertex. The list contains
  * all neighbors in format of Nbr, which contains the other Node and the data on
@@ -268,7 +431,7 @@ class ConstAdjList {
 
  public:
   ConstAdjList() {}
-  ConstAdjList(NbrT* b, NbrT* e) : begin_(b), end_(e) {}
+  ConstAdjList(const NbrT* b, const NbrT* e) : begin_(b), end_(e) {}
   ~ConstAdjList() {}
 
   inline bool Empty() const { return begin_ == end_; }
@@ -340,11 +503,11 @@ class ConstAdjList {
  *
  */
 struct DestList {
-  DestList(fid_t* begin_, fid_t* end_) : begin(begin_), end(end_) {}
+  DestList(const fid_t* begin_, const fid_t* end_) : begin(begin_), end(end_) {}
   inline bool Empty() { return begin == end; }
   inline bool NotEmpty() { return !Empty(); }
-  fid_t* begin;
-  fid_t* end;
+  const fid_t* begin;
+  const fid_t* end;
 };
 
 }  // namespace grape
