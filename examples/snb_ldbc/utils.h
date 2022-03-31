de6c786356c733e1e66087e49c8d6f8ea92d6e52
晓简 <lxj193371@alibaba-inc.com>
2022-03-31 11:21:11 +0800

    split

--- examples/snb_ldbc/utils.h
+++ examples/snb_ldbc/utils.h
@@ -8,9 +8,11 @@
 
 namespace grape {
 
+#if 0
 void split(const char* data,
            std::vector<nonstd::string_view>& splits,
            char delimiter) {
+  splits.clear();
   const char* end = data + strlen(data);
   while (true) {
     const char* ptr = data;
@@ -24,6 +26,22 @@ void split(const char* data,
     data = ptr + 1;
   }
 }
+#else
+void split(const char* data,
+           std::vector<nonstd::string_view>& splits,
+           char delimiter) {
+  if (splits.empty()) {
+    return ;
+  }
+  int num = splits.size() - 1;
+  for (int i = 0; i < num; ++i) {
+    const char* end = strchr(data, ',');
+    splits[i] = nonstd::string_view(data, end - data);
+    data = end + 1;
+  }
+  splits[num] = nonstd::string_view(data, strlen(data));
+}
+#endif
 
 }  // namespace grape
 
